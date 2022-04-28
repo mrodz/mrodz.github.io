cfa001b83f78d20fcd1a0a542276d30e8ff436c0
mrodz <79176075+mrodz@users.noreply.github.com>
2022-04-27 19:28:15 -0700

    Refactoring

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,69 @@
-const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
+import express from 'express';
+import cors from 'cors';
+import fetch, { Headers } from 'node-fetch'
+import { readFile } from 'fs/promises';
+import nodeCron from 'node-cron';
+import { google } from 'googleapis'
 
+/**
+ * @deprecated - too complicated + no documentation + Yelp fucking sucks when it comes to providing reviews.
+ */
+
+const requestEndpoints = {
+    information: "https://api.yelp.com/v3/businesses/comparis-los-angeles",
+    reviews: "https://mybusiness.googleapis.com/v4/compari%27s+trattoria+pizzeria/reviews"
+};
+const mybusinessbusinessinformation = google.mybusinessbusinessinformation('v1').googleLocations();
+mybusinessbusinessinformation.get()
+
+const keys = JSON.parse(await readFile(new URL('../frontend/src/API_SECRET.json', import.meta.url)));
 const PORT = 5000;
 const app = express();
-
 app.use(cors());
 const corsOptions = {
     origin: "http://localhost:3000"
 };
 
-const requestEndpoint = "https://xkcd.com/327/info.0.json";
+const fetchOptions = {
+    method: 'GET',
+    headers: {
+        'Authorization': `Bearer ${keys.yelp}`
+    }
+};
+
+var information = await getInformation();
+var reviews = await getReviews();
+
+// will update the API every 24 hours
+nodeCron.schedule('0 0 0 * * *', async () => {
+    information = await getInformation();
+    reviews = await getReviews();
+});
+
+function getDate() {
+    return (new Date()).toLocaleDateString('en-US');
+}
+
+async function getInformation() {
+    console.log(`[${getDate()}]: GET + ${requestEndpoints.information}`);
+    const response = await fetch(requestEndpoints.information, fetchOptions);
+    return await response.json();
+}
+
+async function getReviews() {
+    console.log(`[${getDate()}]: GET + ${requestEndpoints.reviews}`);
+    const response = await fetch(requestEndpoints.reviews, fetchOptions);
+    return await response.json();
+}
 
 // This function runs if the http://localhost:5000/getData endpoint
 // is requested with a GET request
-app.get('/getData', cors(corsOptions), async (req, res) => {
-    const fetchOptions = {
-        method: 'GET'
-    }
-    const response = await fetch(requestEndpoint, fetchOptions);
-    const jsonResponse = await response.json();
-    res.json(jsonResponse);
+app.get('/yelp/restaurant-info', cors(corsOptions), async (req, res) => {
+    res.status(200).json(information);
+});
+
+app.get('/yelp/restaurant-reviews', cors(corsOptions), async (req, res) => {
+    res.status(200).json(reviews);
 });
 
 app.listen(PORT, () => {
